e6211cf51164773c72c495bed4a984441b465397
a0000778 <a0000778@gmail.com>
2015-09-02 20:09:52 +0800

    重構 channel 調整錯誤處理 調整 API

---
+++ channel.js
@@ -0,0 +1,100 @@
+'use strict';
+var config=require('./config.js');
+var db=require('./db.js');
+
+var channel={};
+var channelList=new Map();
+
+channel.create=function(name,callback){
+	db.createChannel(name,function(channelId){
+		new Channel(channelId,name);
+		callback(channelId);
+	});
+}
+channel.loadAll=function(callback){
+	if(channelList.size) return;
+	db.getAllChannel(function(result){
+		for(let ch of result)
+			new Channel(ch.channelId,ch.name);
+		callback();
+	});
+}
+channel.findById= (channelId) => channelList.get(channelId);
+channel.list=function(){
+	let list=[];
+	for(let ch of channelList.values())
+		list.push(ch);
+	return list;
+}
+channel.send=function(data){
+	if(!(Buffer.isBuffer(data) || typeof(data)==='string'))
+		data=JSON.stringify(data);
+	for(let ch of channelList.values())
+		ch.send(data);
+}
+
+function Channel(channelId,name){
+	this.channelId=channelId;
+	this.name=name;
+	this.lock=false;
+	this.onlineList=new Set();
+	this.onlineMax=config.channelUserMax;
+	channelList.set(this.channelId,this);
+}
+Channel.prototype.delete=function(callback){
+	if(this.onlineList.size)
+		throw new Error('channel not empty')
+	var _=this;
+	db.deleteChannel(this.channelId,function(){
+		channelList.remove(_.channelId);
+		callback();
+	});
+}
+Channel.prototype.exit=function(user){
+	if(this.onlineList.has(user)){
+		this.onlineList.remove(user);
+		user.channel=null;
+		this.send({
+			'action': 'channel_exit',
+			'userId': user.userId
+		});
+	}
+	return this;
+}
+Channel.prototype.join=function(user,force){
+	if(this.lock) return false;
+	if(this.onlineList.has(user)) return true;
+	if(this.onlineMax<=this.onlineList.size && !force) return false;
+	if(user.channel) user.channel.exit(user);
+	this.send({
+		'action': 'channel_join',
+		'userId': user.userId
+	});
+	this.onlineList.add(user);
+	user.channel=this;
+	return true;
+}
+Channel.prototype.list=function(){
+	let list=[];
+	for(let u of this.onlineList)
+		list.push(u);
+	return list;
+}
+Channel.prototype.send=function(data){
+	if(!(Buffer.isBuffer(data) || typeof(data)==='string'))
+		data=JSON.stringify(data);
+	for(let client of channelList.values())
+		client.send(data);
+	return this;
+}
+Channel.prototype.update=function(name,callback){
+	if(name.length===0)
+		throw new Error('field format error');
+	var _=this;
+	db.updateChannel(this.channelId,name,function(){
+		_.name=name;
+		callback();
+	});
+}
+
+module.exports=channel;
